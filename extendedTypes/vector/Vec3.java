41f473caa4c8fee08cc050f710812ac207cea31c
EliteTK <tomasz.kramkowski@gmail.com>
2013-11-30 14:25:46 +0000

    Started work on reworking Vec3

--- extendedTypes/vector/Vec3.java
+++ extendedTypes/vector/Vec3.java
@@ -1,7 +1,5 @@
 /**
- * IMPORTANT: THIS CLASS IS NOT MINE... IT WILL SOON BECOME REPLACED BY ONE
- * ENTIRELY WRITTEN BY ME. I ONCE MADE THE MISTAKE OF DELETING THE ONE I WROTE
- * AND REPLACING IT WITH THIS. UNFORTUNATELY THAT'S NO LEARNING EXPERIENCE.
+ * IMPORTANT: THIS CLASS IS CURRENTLY UNDER RECONSTRUCTION.
  */
 package extendedTypes.vector;
 
@@ -10,137 +8,90 @@ package extendedTypes.vector;
  */
 public class Vec3 {
 
-    float x1 = (float) 0.0, x2 = (float) 0.0, x3 = (float) 0.0;
+    double x1, x2, x3;
 
-    /**
-     * Simply creates instance, components initialised to
-     * <tt>(float)0.0</tt>
-     */
+    //Constructor declarations:
+    //Clean vector
     public Vec3() {
-        //vector already set to (0.0,0.0,0.0)
+        this.x1 = 0;
+        this.x2 = 0;
+        this.x3 = 0;
     }
 
-    public Vec3(float xx1, float xx2, float xx3) {
-        x1 = xx1;
-        x2 = xx2;
-        x3 = xx3;
+    //Vector from values
+    public Vec3(double x1, double x2, double x3) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
     }
 
-    public void setValue(float xx1, float xx2, float xx3) {
-        x1 = xx1;
-        x2 = xx2;
-        x3 = xx3;
+    //Vector from vector
+    public Vec3(Vec3 vec3) {
+        this.x1 = vec3.x1;
+        this.x2 = vec3.x2;
+        this.x3 = vec3.x3;
     }
 
-    public void setValue(Vec3 a) {
-        x1 = a.x1;
-        x2 = a.x2;
-        x3 = a.x3;
+    //Vector from array
+    public Vec3(double[] array) {
+        this.x1 = array[0];
+        this.x2 = array[1];
+        this.x3 = array[3];
     }
 
-    public void setValueRelative(float xx1, float xx2, float xx3) {
-        x1 = x1 + xx1;
-        x2 = x2 + xx2;
-        x3 = x3 + xx3;
-    }
+    //Vector functions(local):
+    /* 
+     * Todo
+     */
+    //Vector functions(static):
+    //Vector length
 
     /**
-     * Sets <tt>Vec3 this</tt> to zero
+     *
+     * @param vec
+     * @return Length of vector vec.
      */
-    public void setZero() {
-        x1 = (float) 0.0;
-        x2 = (float) 0.0;
-        x3 = (float) 0.0;
+        public static double length(Vec3 vec) {
+        return Math.sqrt((vec.x1 * vec.x1) + (vec.x2 * vec.x2) + (vec.x3 * vec.x3));
     }
+
+    //Scalar product
 
     /**
-     * Add and accumulate. <tt>Vec3 a</tt> is added to
-     * <tt>Vec3 this</tt>
+     *
+     * @param vec1
+     * @param vec2
+     * @return Dot (scalar) product of vec1 and vec2.
      */
-    public void accAdd(Vec3 a) {
-        x1 = x1 + a.x1;
-        x2 = x2 + a.x2;
-        x3 = x3 + a.x3;
+        public static double dot(Vec3 vec1, Vec3 vec2) {
+        return (vec1.x1 * vec2.x1) + (vec1.x2 * vec2.x2) + (vec1.x3 * vec2.x3);
     }
+
+    //Vector product
 
     /**
-     * Subtract and accumulate. <tt>Vec3 a</tt> is subtracted from
-     * <tt>Vec3 this</tt>
+     *
+     * @param vec1
+     * @param vec2
+     * @return Cross (vector) product of vec1 and vec2.
      */
-    public void accSub(Vec3 a) {
-        x1 = x1 - a.x1;
-        x2 = x2 - a.x2;
-        x3 = x3 - a.x3;
+        public static Vec3 cross(Vec3 vec1, Vec3 vec2) {
+        double i, j, k;
+        i = (vec1.x2 * vec2.x3 - vec1.x3 * vec2.x2);
+        j = (vec1.x3 * vec2.x1 - vec1.x1 * vec2.x3);
+        k = (vec1.x1 * vec2.x2 - vec1.x2 * vec2.x1);
+        return new Vec3(i, j, k);
     }
 
-    public static float dot(Vec3 a, Vec3 b) {
-        return (a.x1 * b.x1 + a.x2 * b.x2 + a.x3 * b.x3);
-    }
-
-    public void cross(Vec3 a, Vec3 b) {
-        x1 = a.x2 * b.x3 - a.x3 * b.x2;
-        x2 = a.x3 * b.x1 - a.x1 * b.x3;
-        x3 = a.x1 * b.x2 - a.x2 * b.x1;
-    }
-
-    public void normal(Vec3 a, Vec3 b) {
-        // this is normal to a and b
-        float length;
-        x1 = a.x2 * b.x3 - a.x3 * b.x2;
-        x2 = a.x3 * b.x1 - a.x1 * b.x3;
-        x3 = a.x1 * b.x2 - a.x2 * b.x1;
-        length = this.length();
-        x1 = x1 / length;
-        x2 = x2 / length;
-        x3 = x3 / length;
-    }
+    //Normalize (set length to 1)
 
     /**
-     * Calculate unit normal to triangle given by position vectors a, b and c in
-     * the sense that a->b->c goes right-handed around n. For example, if a, b
-     * and c are clockwise and flat in the plane of view, n is away from the
-     * viewer.<p>
-     * Alternatively, this is (b-a) cross (c-a) normalised.
+     *
+     * @param vec
+     * @return The normalized vector of vec. (sets length to 1)
      */
-    public void normal(Vec3 a, Vec3 b, Vec3 c) {
-        x1 = (b.x2 - a.x2) * (c.x3 - a.x3) - (b.x3 - a.x3) * (c.x2 - a.x2);
-        x2 = (b.x3 - a.x3) * (c.x1 - a.x1) - (b.x1 - a.x1) * (c.x3 - a.x3);
-        x3 = (b.x1 - a.x1) * (c.x2 - a.x2) - (b.x2 - a.x2) * (c.x1 - a.x1);
-        float length = this.length();
-        x1 = x1 / length;
-        x2 = x2 / length;
-        x3 = x3 / length;
+        public static Vec3 normalize(Vec3 vec) {
+        double length = length(vec);
+        return new Vec3(vec.x1 / length, vec.x2 / length, vec.x3 / length);
     }
-
-    public void normalise(Vec3 a) {
-        float length;
-        length = a.length();
-        a.x1 = a.x1 / length;
-        a.x2 = a.x2 / length;
-        a.x3 = a.x3 / length;
-    }
-
-    /**
-     * Returns length of vector
-     */
-    public float length() {
-        return ((float) Math.sqrt(x1 * x1 + x2 * x2 + x3 * x3));
-    }
-
-    /**
-     * Returns length of vector squared
-     */
-    public float lengthSqd() {
-        return (x1 * x1 + x2 * x2 + x3 * x3);
-    }
-
-    /**
-     * Returns length of vector (b-a) squared
-     */
-    public static float distSqd(Vec3 a, Vec3 b) {
-        return ((float) (a.x1 - b.x1) * (a.x1 - b.x1)
-                + (a.x2 - b.x2) * (a.x2 - b.x2)
-                + (a.x3 - b.x3) * (a.x3 - b.x3));
-    }
-
 }
