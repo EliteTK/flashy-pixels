e505300de5fab2ee7f11f4c4e038f389f83c9a23
EliteTK <tomasz.kramkowski@gmail.com>
2013-11-30 21:49:47 +0000

    Started work on the renderer. Names are temporary and are subject to change. Still trying to figure out exactly how this should all look.

---
+++ renderer/vertexManager/PolyStack.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package renderer.vertexManager;
+
+import extendedTypes.polygon.Poly3;
+
+/**
+ *
+ * @author main
+ */
+public class PolyStack {
+
+    private final int size;
+    private final Poly3[] polyBuffer;
+    private int pointer = 0;
+
+    public PolyStack(int size) {
+        this.size = size;
+        this.polyBuffer = new Poly3[size];
+    }
+
+    public void push(Poly3 poly) {
+        if (pointer < size) {
+            this.polyBuffer[pointer++] = poly;
+        } else {
+            System.err.println("PolyStack overflow.");
+        }
+    }
+
+    public Poly3 pop() {
+        if (pointer > 0) {
+            return this.polyBuffer[--pointer];
+        } else {
+            System.err.println("PolyStack underflow");
+            return new Poly3();
+        }
+    }
+}
