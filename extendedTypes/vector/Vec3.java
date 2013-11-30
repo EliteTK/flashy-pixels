f0df2f2fb8d3c3f5f5ef6cf761421c58d86223f3
EliteTK <tomasz.kramkowski@gmail.com>
2013-11-30 15:06:15 +0000

    Fixed normal function of Vec3 and added some set value functions.

--- extendedTypes/vector/Vec3.java
+++ extendedTypes/vector/Vec3.java
@@ -10,7 +10,6 @@ public class Vec3 {
 
     double x1, x2, x3;
 
-    //Constructor declarations:
     //Clean vector
     public Vec3() {
         this.x1 = 0;
@@ -26,10 +25,10 @@ public class Vec3 {
     }
 
     //Vector from vector
-    public Vec3(Vec3 vec3) {
-        this.x1 = vec3.x1;
-        this.x2 = vec3.x2;
-        this.x3 = vec3.x3;
+    public Vec3(Vec3 vec) {
+        this.x1 = vec.x1;
+        this.x2 = vec.x2;
+        this.x3 = vec.x3;
     }
 
     //Vector from array
@@ -39,10 +38,61 @@ public class Vec3 {
         this.x3 = array[3];
     }
 
-    public void setValue() {
-
+    /**
+     *
+     * @param x1
+     * @param x2
+     * @param x3
+     * Sets the values of this to 3 new values.
+     */
+    public void setValue(double x1, double x2, double x3) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
     }
     
+    /**
+     *
+     * @param vec
+     * Sets the value of this to another vector.
+     */
+    public void setValue(Vec3 vec) {
+        this.x1 = vec.x1;
+        this.x2 = vec.x2;
+        this.x3 = vec.x3;
+    }
+    
+    /**
+     *
+     * @param array
+     * Sets the values of this to the 3 values of a length 3+ array.
+     */
+    public void setValue(double[] array) {
+        this.x1 = array[0];
+        this.x2 = array[1];
+        this.x3 = array[3];
+    }
+
+    /**
+     *
+     * @param vec1
+     * @param vec2
+     * @return Vector subtraction: vec1 - vec2 .
+     */
+    public static Vec3 subtract(Vec3 vec1, Vec3 vec2) {
+        return new Vec3((vec1.x1 - vec2.x1), (vec1.x2 - vec2.x2), (vec1.x3 - vec2.x3));
+    }
+
+    /**
+     *
+     * @param vec1
+     * @param vec2
+     * @return Vector addition: vec1 + vec2 .
+     */
+    public static Vec3 add(Vec3 vec1, Vec3 vec2) {
+        return new Vec3((vec1.x1 + vec2.x1), (vec1.x2 + vec2.x2), (vec1.x3 + vec2.x3));
+    }
+
     /**
      *
      * @param vec
@@ -86,7 +136,14 @@ public class Vec3 {
         return new Vec3(vec.x1 / length, vec.x2 / length, vec.x3 / length);
     }
 
-    public static Vec3 normal(Vec3 vec1, Vec3 vec2) {
-        return new Vec3(normalize(cross(vec1, vec2)));
+    /**
+     *
+     * @param vec1
+     * @param vec2
+     * @param vec3
+     * @return Takes the normal to 3 positional vectors.
+     */
+    public static Vec3 normal(Vec3 vec1, Vec3 vec2, Vec3 vec3) {
+        return new Vec3(normalize(cross(subtract(vec2, vec1), subtract(vec3, vec1))));
     }
 }
