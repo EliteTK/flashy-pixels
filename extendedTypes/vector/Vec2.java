4277a7ffebbaaa1647dfa591a0e5b26bb139ddcc
EliteTK <tomasz.kramkowski@gmail.com>
2013-11-30 16:20:34 +0000

    Standardised Vec2 so it is more like Vec3.

--- extendedTypes/vector/Vec2.java
+++ extendedTypes/vector/Vec2.java
@@ -2,13 +2,14 @@ package extendedTypes.vector;
 
 public class Vec2 {
 
-    float x1 = (float) 0.0, x2 = (float) 0.0;
+    double x1, x2;
 
     public Vec2() {
-
+        this.x1 = 0;
+        this.x2 = 0;
     }
 
-    public Vec2(float x1, float x2) {
+    public Vec2(double x1, double x2) {
         this.x1 = x1;
         this.x2 = x2;
     }
@@ -18,18 +19,18 @@ public class Vec2 {
         this.x2 = vec2.x2;
     }
 
-    public void setValue(float x1, float x2) {
+    public void setValue(double x1, double x2) {
         this.x1 = x1;
         this.x2 = x2;
     }
 
-    public void setValue(Vec2 vec2) {
-        this.x1 = vec2.x1;
-        this.x2 = vec2.x2;
+    public void setValue(Vec2 vec) {
+        this.x1 = vec.x1;
+        this.x2 = vec.x2;
     }
 
-    public float length() {
-        return (float) Math.sqrt(x1 * x1 + x2 * x2);
+    public static double length(Vec2 vec) {
+        return Math.sqrt(vec.x1 * vec.x1 + vec.x2 * vec.x2);
     }
 
     public static Vec2 add(Vec2 vec1, Vec2 vec2) {
@@ -39,7 +40,7 @@ public class Vec2 {
         return vec3;
     }
 
-    public static Vec2 sub(Vec2 vec1, Vec2 vec2) {
+    public static Vec2 subtract(Vec2 vec1, Vec2 vec2) {
         Vec2 vec3 = new Vec2();
         vec3.x1 = vec1.x1 - vec2.x1;
         vec3.x2 = vec1.x2 - vec2.x2;
@@ -47,7 +48,7 @@ public class Vec2 {
     }
 
     public static Vec2 normalize(Vec2 vec) {
-        float length = vec.length();
+        double length = Vec2.length(vec);
         vec.x1 = vec.x1 / length;
         vec.x2 = vec.x2 / length;
         return vec;
