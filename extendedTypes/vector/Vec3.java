0f38d182110bbf7af5a69b8ad56143c3d5be883f
EliteTK <tomasz.kramkowski@gmail.com>
2013-11-30 14:48:14 +0000

    Added normal vector function to Vec3

--- extendedTypes/vector/Vec3.java
+++ extendedTypes/vector/Vec3.java
@@ -39,23 +39,18 @@ public class Vec3 {
         this.x3 = array[3];
     }
 
-    //Vector functions(local):
-    /* 
-     * Todo
-     */
-    //Vector functions(static):
-    //Vector length
+    public void setValue() {
 
+    }
+    
     /**
      *
      * @param vec
      * @return Length of vector vec.
      */
-        public static double length(Vec3 vec) {
+    public static double length(Vec3 vec) {
         return Math.sqrt((vec.x1 * vec.x1) + (vec.x2 * vec.x2) + (vec.x3 * vec.x3));
     }
-
-    //Scalar product
 
     /**
      *
@@ -63,11 +58,9 @@ public class Vec3 {
      * @param vec2
      * @return Dot (scalar) product of vec1 and vec2.
      */
-        public static double dot(Vec3 vec1, Vec3 vec2) {
+    public static double dot(Vec3 vec1, Vec3 vec2) {
         return (vec1.x1 * vec2.x1) + (vec1.x2 * vec2.x2) + (vec1.x3 * vec2.x3);
     }
-
-    //Vector product
 
     /**
      *
@@ -75,7 +68,7 @@ public class Vec3 {
      * @param vec2
      * @return Cross (vector) product of vec1 and vec2.
      */
-        public static Vec3 cross(Vec3 vec1, Vec3 vec2) {
+    public static Vec3 cross(Vec3 vec1, Vec3 vec2) {
         double i, j, k;
         i = (vec1.x2 * vec2.x3 - vec1.x3 * vec2.x2);
         j = (vec1.x3 * vec2.x1 - vec1.x1 * vec2.x3);
@@ -83,15 +76,17 @@ public class Vec3 {
         return new Vec3(i, j, k);
     }
 
-    //Normalize (set length to 1)
-
     /**
      *
      * @param vec
      * @return The normalized vector of vec. (sets length to 1)
      */
-        public static Vec3 normalize(Vec3 vec) {
+    public static Vec3 normalize(Vec3 vec) {
         double length = length(vec);
         return new Vec3(vec.x1 / length, vec.x2 / length, vec.x3 / length);
+    }
+
+    public static Vec3 normal(Vec3 vec1, Vec3 vec2) {
+        return new Vec3(normalize(cross(vec1, vec2)));
     }
 }
