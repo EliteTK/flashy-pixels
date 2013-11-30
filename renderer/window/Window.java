092ed65a76f6447df0944d470b8db187d8bfed03
EliteTK <tomasz.kramkoski@gmail.com>
2013-11-30 13:19:13 +0000

    Removed frame creation from constructor so that "this" can be used more safely.

--- renderer/window/Window.java
+++ renderer/window/Window.java
@@ -13,18 +13,24 @@ public class Window extends Canvas {
     JFrame frame;
     public int width;
     public int height;
+    public String title;
 
     public Window(int width, int height, String title) {
         this.width = width;
         this.height = height;
+        this.title = title;
 
-        Dimension size = new Dimension(width, height);
+        Dimension size;
+        size = new Dimension(width, height);
+        
         setPreferredSize(size);
+    }
 
+    public void createFrame() {
         frame = new JFrame();
 
         frame.setResizable(false);
-        frame.setTitle(title);
+        frame.setTitle(this.title);
         frame.add(this);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
